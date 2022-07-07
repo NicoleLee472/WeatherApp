d21867477254c1a6bb7c8e9d0357b1c66eca9d29
NicoleLee472 <62321810+NicoleLee472@users.noreply.github.com>
2022-07-07 12:30:17 +1000

    Put querySelectors in objects

--- src/script.js
+++ src/script.js
@@ -42,30 +42,32 @@ let Dates = (function () {
   let now = function () {
     return new Date()
   }
-  let currentDayNumeric = now().getDay
+  let currentDay = now().getDay()
+
+  const optionsDate = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  }
 
   const optionsDayAndTime = {
     weekday: 'short',
     hour: 'numeric',
     minute: '2-digit',
   }
-  const optionsDate = {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  }
+
   let refreshDates = function () {
     let currentTime = now()
     currentDate.innerHTML = currentTime.toLocaleDateString('en-AU', optionsDate)
     currentDayAndTime.innerHTML = currentTime
       .toLocaleDateString('en-Au', optionsDayAndTime)
       .replace(',', ' ')
-    currentDayNumeric = now().getDay
+    currentDay = currentTime.getDay()
   }
   refreshDates()
   return {
     displayDates: refreshDates,
-    currentDay: currentDayNumeric,
+    currentDayNumeric: currentDay,
   }
 })()
 
@@ -73,25 +75,40 @@ let SearchResults = (function () {
   const apiKey = '18e21a300ebbb03dff744f074bc3f64f'
   const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?`
 
+  let mutateElements = {
+    chosenCity: document.querySelector('#chosenCity'),
+    stateAndCountry: document.querySelector('#stateAndCountry'),
+    currentWeatherEmoji: document.querySelector('#currentWeatherEmoji'),
+    currentHumidity: document.querySelector('#currentHumidity'),
+    currentWind: document.querySelector('#currentWind'),
+    currentDescription: document.querySelector('#currentDescription'),
+  }
+
+  let actionElements = {
+    requestedCity: document.querySelector('#requestedCity'),
+    searchFormSubmission: document.querySelector('form'),
+  }
+
   let showCurrentData = function (response) {
     Temperatures.setDegreesCelsius(response.data.main.temp)
 
-    chosenCity.innerHTML = response.data.name
-    stateAndCountry.innerHTML = response.data.sys.country
-    currentWeatherEmoji.src = `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-    currentHumidity.innerHTML = `Humidity: ${response.data.main.humidity}%`
-    currentWind.innerHTML = `Wind speed: ${response.data.wind.speed} m/s`
-    currentDescription.innerHTML = response.data.weather[0].description
+    mutateElements.chosenCity.innerHTML = response.data.name
+    mutateElements.stateAndCountry.innerHTML = response.data.sys.country
+    mutateElements.currentWeatherEmoji.src = `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    mutateElements.currentHumidity.innerHTML = `Humidity: ${response.data.main.humidity}%`
+    mutateElements.currentWind.innerHTML = `Wind speed: ${response.data.wind.speed} m/s`
+    mutateElements.currentDescription.innerHTML =
+      response.data.weather[0].description
   }
-
-  let requestedCity = document.querySelector('#requestedCity')
 
   let searchApiCall = function (event) {
     event.preventDefault()
     axios
-      .get(`${weatherUrl}q=${requestedCity.value}&appid=${apiKey}&units=metric`)
+      .get(
+        `${weatherUrl}q=${actionElements.requestedCity.value}&appid=${apiKey}&units=metric`
+      )
       .then(showCurrentData)
   }
-  let searchFormSubmission = document.querySelector('form')
-  searchFormSubmission.addEventListener('submit', searchApiCall)
+
+  actionElements.searchFormSubmission.addEventListener('submit', searchApiCall)
 })()
